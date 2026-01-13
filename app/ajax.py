87c1f0702c80fc7175397b76a139e60187808ba5
Marin <marinnagy74@gmail.com>
2026-01-13 18:42:31 +0100

    Improved recording + record UI upgrade

--- app/ajax.py
+++ app/ajax.py
@@ -8,7 +8,7 @@ front end. No complex logic.
 from typing import cast
 
 from flask import Blueprint, current_app
-from flask_sock import Sock
+from flask_sock import Sock, ConnectionClosed
 
 from .init import AppContext
 
@@ -27,10 +27,14 @@ sock = Sock()
 
 @sock.route('/audio_stt')
 def audio_stt(ws):
+    print('starting websocket')
     with open("received_audio.webm", "wb") as f:   # For testing only
-        while True:
-            data = ws.receive()
-            if data is None:
-                break
-            f.write(data)
-    print("Audio stream ended")
+        try:
+            while True:
+                print('received')
+                data = ws.receive()
+                if data is None:
+                    break
+                f.write(data)
+        except ConnectionClosed:
+            print("Audio stream ended")
